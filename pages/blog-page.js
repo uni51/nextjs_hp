a0e178e92b387095fcbbbf634ab88097791a3ec5
Koichi.Shimomura <unieye51@gmail.com>
2022-06-02 12:01:51 +0900

    2-11 getStaticProps

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,11 +1,23 @@
-import Layout from "../components/Layout"
+import Layout from "../components/Layout";
+import Post from "../components/Post";
+import { getAllPostsData } from "../lib/posts";
 
-const Blog = () => {
+const Blog = ({ posts }) => {
 	return (
 		<Layout title="Blog">
-			Blog Page
+			<ul className="m-10">
+				{posts && posts.map((post) => <Post key={post.id} post={post} />)}
+			</ul>
 		</Layout>
 	)
 }
 
 export default Blog
+
+// ビルド時にサーバサイドで一度だけ実行される
+export async function getStaticProps() {
+	const posts = await getAllPostsData();
+	return {
+		props: { posts }
+	};
+}
